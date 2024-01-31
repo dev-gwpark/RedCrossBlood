d8cd94a0953520483e88792828738c610326abfb
박건우 <geonwoo@baggeon-uui-MacBookAir.local>
2024-01-31 21:32:13 +0900

    Initial commit

---
+++ src/main/java/com/rcb/lims/RedCrossBlood/RedCrossBloodApplication.java
@@ -0,0 +1,13 @@
+package com.rcb.lims.RedCrossBlood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedCrossBloodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedCrossBloodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcb/lims/RedCrossBlood/RedCrossBloodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcb.lims.RedCrossBlood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedCrossBloodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
